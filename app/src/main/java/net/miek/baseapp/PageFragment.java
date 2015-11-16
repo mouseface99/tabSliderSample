ac2213c6a5b906ae3b83eb22e2941744cbe89912
Mike Wang <Mike.Wang@fuhu.com>
2015-11-16 14:20:11 +0800

    Add SwipeRefresh features (Pull to Refresh)

--- app/src/main/java/net/miek/baseapp/PageFragment.java
+++ app/src/main/java/net/miek/baseapp/PageFragment.java
@@ -1,10 +1,10 @@
 package net.miek.baseapp;
 
-import android.graphics.Color;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.Log;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,22 +13,48 @@ import android.widget.TextView;
 /**
  * Created by Mike Wang on 2015/11/12.
  */
-public class PageFragment extends Fragment {
+public class PageFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
 
+    private SwipeRefreshLayout swipeLayout;
+    private TextView mTV;
+    private String info;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.pager_fragment, container, false);
 
-        TextView tv = (TextView)root.findViewById(R.id.pager_info);
+        mTV = (TextView) root.findViewById(R.id.pager_info);
 
         int color = Utils.getColor();
         root.setBackgroundColor(color);
-        tv.setTextColor((0xFFFFFFFF - color) + 0xFF000000);
+        mTV.setTextColor((0xFFFFFFFF - color) + 0xFF000000);
 
-        if(tv != null)
-            tv.setText("This is " + getArguments().getString("page_info"));
+        info = getArguments().getString("page_info");
+
+        if (mTV != null)
+            mTV.setText("This is " + info);
+
+
+        swipeLayout = (SwipeRefreshLayout) root.findViewById(R.id.swipe_container);
+        swipeLayout.setOnRefreshListener(this);
+        swipeLayout.setColorSchemeResources(
+                android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
 
         return root;
+    }
+
+    @Override
+    public void onRefresh() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mTV.setText("This is " + info);
+                swipeLayout.setRefreshing(false);
+            }
+        }, 5000);
+        mTV.setText("onRefresh !!!!!");
     }
 }
